79f2c98e90f5de9895149147c924b77a82ff768f
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-22 11:36:09 -0400

    Starting Section 10.146

--- Section-10: The Document Object Model (DOM)/DOM/index.js
+++ Section-10: The Document Object Model (DOM)/DOM/index.js
@@ -1,31 +1,41 @@
-// alert('hello');
+// // alert('hello');
 
-document.querySelector('h1').innerHTML = 'Good Bye';
+// document.querySelector('h1').innerHTML = 'Good Bye';
 
-// GETS ALL MATCHING ELEMENTS AND RETURNS AN ARRAY
-console.log((document.getElementsByTagName('li')[2].style.color = 'red'));
+// // GETS ALL MATCHING ELEMENTS AND RETURNS AN ARRAY
+// console.log((document.getElementsByTagName('li')[2].style.color = 'red'));
 
-console.log(document.getElementsByTagName('li').length);
+// console.log(document.getElementsByTagName('li').length);
 
-console.log((document.getElementsByClassName('btn')[0].style.color = 'orange'));
+// console.log((document.getElementsByClassName('btn')[0].style.color = 'orange'));
 
-// GETS THE SINGLE ID IN THE HTML ELEMENT
-console.log((document.getElementById('title').innerHTML = 'YES'));
+// // GETS THE SINGLE ID IN THE HTML ELEMENT
+// console.log((document.getElementById('title').innerHTML = 'YES'));
 
-console.log((document.querySelector('ul').style.border = '1rem solid red'));
+// console.log((document.querySelector('ul').style.border = '1rem solid red'));
 
-console.log((document.querySelector('#title').style.border = '1rem solid red'));
+// console.log((document.querySelector('#title').style.border = '1rem solid red'));
 
-console.log((document.querySelector('li a').style.fontSize = '3rem'));
+// console.log((document.querySelector('li a').style.fontSize = '3rem'));
 
-console.log(document.querySelector('li.item'));
-console.log(document.querySelector('#list a'));
+// console.log(document.querySelector('li.item'));
+// console.log(document.querySelector('#list a'));
 
-// RETURNS A LIST OF ALL MATCHING ELEMENTS
-console.log(document.querySelectorAll('#list .item'));
+// // RETURNS A LIST OF ALL MATCHING ELEMENTS
+// console.log(document.querySelectorAll('#list .item'));
 
-// querySelector and querySelector provide the most flexibility and the most commonly used
+// // querySelector and querySelector provide the most flexibility and the most commonly used
 
-// CHALLENGE
+// // CHALLENGE
 
-document.querySelector('#list a').style.color = 'red';
+// document.querySelector('#list a').style.color = 'red';
+
+document.querySelector('h1').style.color = 'red';
+
+document.querySelector('h1').style.fontSize = '3rem';
+
+document.querySelector('h1').style.padding = '1rem 5rem';
+
+// document.querySelector('h1').style.visibility = 'hidden';
+
+document.querySelector('.btn').style.backgroundColor = 'yellow';
