123d2d9e1895e5459cd9441f4bdd8af394ba2e00
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-23 12:29:24 -0400

    starting 12.167

--- Section-12: Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Section-12: Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,7 +1,28 @@
 let drums = document.querySelectorAll('.drum');
 
-drums.forEach((drum) =>
-  drum.addEventListener('click', function () {
-    alert('I was clicked');
-  })
-);
+drums.forEach((drum) => drum.addEventListener('click', iGotClicked));
+
+function iGotClicked() {
+  alert('I was clicked');
+}
+
+// CALCULATOR FUNCTIONS
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
