d42e3fc72cc7f810a4002b57960da4914b07dd88
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-01 16:06:02 -0400

    Section 17 Complete

--- Section-17: Node.js/intro-to-node/index.js
+++ Section-17: Node.js/intro-to-node/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+const randomHero = superheroes.all.length;
+const randomVillains = supervillains.all.length;
+console.log(`${randomHero} Heroes`);
+console.log(`${randomVillains} Villains`);
