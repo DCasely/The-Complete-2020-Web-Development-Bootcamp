d7575787be2cc7bab369a3d6db43567fadf3e9de
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-01 13:43:02 -0400

    section 244

--- Section-18: Express.js with Node.js/my-express-server/server.js
+++ Section-18: Express.js with Node.js/my-express-server/server.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>Hello, World!</h1>'));
+app.get('/', (req, res) => {
+  res.send('<h1>Hello, World!</h1>');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact me at: davin@davincasely.com');
+});
+
+app.get('/about', (req, res) => {
+  res.send('I am Davin Casely.');
+});
+
+app.get('/hobbies', (req, res) => {
+  res.send('Hobbies');
+});
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
