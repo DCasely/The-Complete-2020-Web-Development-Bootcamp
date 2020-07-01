949cf14a35754b37dd74cae4b0dbc8b8332b72d9
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-01 16:43:50 -0400

    section 254

--- Section-18: Express.js with Node.js/Calculator/calculator.js
+++ Section-18: Express.js with Node.js/Calculator/calculator.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// SIMPLE CALCULATOR
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,10 +13,23 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const num1 = Number(req.body.n1);
   const num2 = Number(req.body.n2);
-
   const result = num1 + num2;
-
   res.send(`The result of the calculation is ${result}`);
+});
+
+// BMI CALCULATOR
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmicalculator.html');
+});
+
+app.post('/bmicalculator', (req, res) => {
+  const weight = Number(req.body.weight);
+  const height = Number(req.body.height);
+  const weightKgs = weight * 0.45359237;
+  const heightMeters = height * 0.0254;
+  const BMI = (weightKgs / Math.pow(heightMeters, 2)).toFixed(2);
+
+  res.send(`Your BMI: ${BMI}`);
 });
 
 app.listen(port, () => console.log(`App listening on port:${port}`));
