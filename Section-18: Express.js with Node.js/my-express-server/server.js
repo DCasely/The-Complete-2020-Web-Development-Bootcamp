2dda014139781a853326f258933c53bfd33087fc
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-01 13:31:17 -0400

    section 243

--- Section-18: Express.js with Node.js/my-express-server/server.js
+++ Section-18: Express.js with Node.js/my-express-server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => res.send('<h1>Hello, World!</h1>'));
+
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
 );
-
-// app.get('/', (req, res) => res.send('Hello World'));
