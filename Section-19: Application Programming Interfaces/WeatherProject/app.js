2867f48474b45d4e64c689a01881de16ed1f32b0
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-02 12:16:46 -0400

    section 260

--- Section-19: Application Programming Interfaces/WeatherProject/app.js
+++ Section-19: Application Programming Interfaces/WeatherProject/app.js
@@ -15,12 +15,17 @@ app.get('/', (req, res) => {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-      console.log(`Temperature: ${temp} and ${description}`);
+      res.write(
+        `<h1>The temperature in Miami is ${temp} degrees Fahrenheit</h1>`
+      );
+      res.write(`<h2>The weather is currently ${description}</h2>`);
+      res.write(`<img src="${imageURL}" alt="weather icon">`);
+      res.send();
     });
   });
-
-  res.send('Server is up and running');
 });
 
 app.listen(port, () => {
