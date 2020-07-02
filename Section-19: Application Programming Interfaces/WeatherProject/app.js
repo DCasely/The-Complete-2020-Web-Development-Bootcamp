3a1299d63ba059af101341fd16a42f05140464db
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-02 11:53:10 -0400

    section 259

--- Section-19: Application Programming Interfaces/WeatherProject/app.js
+++ Section-19: Application Programming Interfaces/WeatherProject/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const https = require('https');
+
 const app = express();
 const port = 3000;
 
@@ -8,7 +9,15 @@ app.get('/', (req, res) => {
     'https://api.openweathermap.org/data/2.5/weather?q=miami&appid=be16e69e39182d44b256aa71dd40da36&units=imperial';
 
   https.get(url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on('data', (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+
+      console.log(`Temperature: ${temp} and ${description}`);
+    });
   });
 
   res.send('Server is up and running');
