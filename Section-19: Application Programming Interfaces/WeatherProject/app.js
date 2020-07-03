8d2d197dbe87af60e2c69de6a4437175e6a7d6dd
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-02 20:26:52 -0400

    section 261

--- Section-19: Application Programming Interfaces/WeatherProject/app.js
+++ Section-19: Application Programming Interfaces/WeatherProject/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=miami&appid=be16e69e39182d44b256aa71dd40da36&units=imperial';
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  const city = req.body.cityName;
+  const apiKey = 'be16e69e39182d44b256aa71dd40da36';
+  const units = 'imperial';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
   https.get(url, function (response) {
     console.log(response.statusCode);
@@ -19,7 +28,7 @@ app.get('/', (req, res) => {
       const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
       res.write(
-        `<h1>The temperature in Miami is ${temp} degrees Fahrenheit</h1>`
+        `<h1>The temperature in ${city} is ${temp} degrees Fahrenheit</h1>`
       );
       res.write(`<h2>The weather is currently ${description}</h2>`);
       res.write(`<img src="${imageURL}" alt="weather icon">`);
