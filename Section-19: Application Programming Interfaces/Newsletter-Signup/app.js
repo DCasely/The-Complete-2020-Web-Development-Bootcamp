ec6fe5efad1cd113b3933f825a32f640cd09e496
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-03 11:05:05 -0400

    section 263

---
+++ Section-19: Application Programming Interfaces/Newsletter-Signup/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
